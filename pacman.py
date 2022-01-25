562a313496b2f4a475b7916c14bae384797c745a
Misha <mihailiy05@icloud.com>
2022-01-25 20:21:04 +0300

    Поменялись персонажи, элементы создаются по координатам

--- pacman.py
+++ pacman.py
@@ -12,6 +12,8 @@ screen = pygame.display.set_mode(size)
 all_sprites = pygame.sprite.Group()
 horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
+arcade_font = pygame.font.Font('data/fonts/arcade-n.ttf', 20)
+background = pygame.image.load("data/GUI/background.png")
 SCORE = 0
 FPS = 60
 clock = pygame.time.Clock()
@@ -41,14 +43,15 @@ def terminate():
 class Player(pygame.sprite.Sprite):
     image = load_image("pacman.png")
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, *param):
+        super().__init__(param[:-2])
         self.image = Player.image
         self.image.set_colorkey(pygame.Color('white'))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = 200
-        self.rect.y = 200
+        self.rect.x = param[-2]
+        self.rect.y = param[-1]
+        self.speed = 3
         self.const = (0, '')
 
     def update(self, *args):
@@ -56,8 +59,8 @@ class Player(pygame.sprite.Sprite):
             self.const = (0, '')
             if self.rect.y <= 5:
                 self.rect.y = 6
-            elif self.rect.y >= 354:
-                self.rect.y = 353
+            elif self.rect.y >= 454:
+                self.rect.y = 453
         if pygame.sprite.spritecollideany(self, vertical_borders):
             self.const = (0, '')
             if self.rect.x <= 5:
@@ -67,19 +70,19 @@ class Player(pygame.sprite.Sprite):
         if args and args[0].key == pygame.K_DOWN:
             self.image = pygame.transform.rotate(Player.image, 270)
             self.image.set_colorkey(pygame.Color('white'))
-            self.const = (3, 'y')
+            self.const = (self.speed, 'y')
         elif args and args[0].key == pygame.K_UP:
             self.image = pygame.transform.rotate(Player.image, 90)
             self.image.set_colorkey(pygame.Color('white'))
-            self.const = (-3, 'y')
+            self.const = (self.speed * -1, 'y')
         elif args and args[0].key == pygame.K_LEFT:
             self.image = pygame.transform.flip(Player.image, True, False)
             self.image.set_colorkey(pygame.Color('white'))
-            self.const = (-3, 'x')
+            self.const = (self.speed * -1, 'x')
         elif args and args[0].key == pygame.K_RIGHT:
             self.image = Player.image
             self.image.set_colorkey(pygame.Color('white'))
-            self.const = (3, 'x')
+            self.const = (self.speed, 'x')
         a, b = self.const
         if b == 'x':
             self.rect.x += a
@@ -91,14 +94,14 @@ class Red(pygame.sprite.Sprite):
     image = load_image("red.png")
     new_image = load_image("edible_ghost.png")
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, *param):
+        super().__init__(param[:-2])
         self.image = Red.image
         self.image.set_colorkey(pygame.Color('white'))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = 10
-        self.rect.y = 10
+        self.rect.x = param[-2]
+        self.rect.y = param[-1]
         self.turn = random.randint(1, 4)
         self.counter = 0
         self.edible = False
@@ -114,8 +117,8 @@ class Red(pygame.sprite.Sprite):
             self.turn = random.choice([2, 4])
             if self.rect.y <= 5:
                 self.rect.y = 6
-            elif self.rect.y >= 352:
-                self.rect.y = 351
+            elif self.rect.y >= 452:
+                self.rect.y = 451
         elif pygame.sprite.spritecollideany(self, vertical_borders):
             self.turn = random.choice([1, 3])
             if self.rect.x <= 5:
@@ -140,14 +143,14 @@ class Green(pygame.sprite.Sprite):
     image = load_image("green.png")
     new_image = load_image("edible_ghost.png")
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, *param):
+        super().__init__(param[:-2])
         self.image = Green.image
         self.image.set_colorkey(pygame.Color('white'))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = 70
-        self.rect.y = 10
+        self.rect.x = param[-2]
+        self.rect.y = param[-1]
         self.turn = random.randint(1, 4)
         self.counter = 0
         self.edible = False
@@ -163,8 +166,8 @@ class Green(pygame.sprite.Sprite):
             self.turn = random.choice([2, 4])
             if self.rect.y <= 5:
                 self.rect.y = 6
-            elif self.rect.y >= 352:
-                self.rect.y = 351
+            elif self.rect.y >= 452:
+                self.rect.y = 451
         elif pygame.sprite.spritecollideany(self, vertical_borders):
             self.turn = random.choice([1, 3])
             if self.rect.x <= 5:
@@ -189,14 +192,14 @@ class Yellow(pygame.sprite.Sprite):
     image = load_image("yellow.png")
     new_image = load_image("edible_ghost.png")
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, *param):
+        super().__init__(param[:-2])
         self.image = Yellow.image
         self.image.set_colorkey(pygame.Color('white'))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = 130
-        self.rect.y = 10
+        self.rect.x = param[-2]
+        self.rect.y = param[-1]
         self.turn = random.randint(1, 4)
         self.counter = 0
         self.edible = False
@@ -212,8 +215,8 @@ class Yellow(pygame.sprite.Sprite):
             self.turn = random.choice([2, 4])
             if self.rect.y <= 5:
                 self.rect.y = 6
-            elif self.rect.y >= 352:
-                self.rect.y = 351
+            elif self.rect.y >= 452:
+                self.rect.y = 451
         elif pygame.sprite.spritecollideany(self, vertical_borders):
             self.turn = random.choice([1, 3])
             if self.rect.x <= 5:
@@ -238,14 +241,14 @@ class Blue(pygame.sprite.Sprite):
     image = load_image("blue.png")
     new_image = load_image("edible_ghost.png")
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, *param):
+        super().__init__(param[:-2])
         self.image = Blue.image
         self.image.set_colorkey(pygame.Color('white'))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = 190
-        self.rect.y = 10
+        self.rect.x = param[-2]
+        self.rect.y = param[-1]
         self.turn = random.randint(1, 4)
         self.counter = 0
         self.edible = False
@@ -261,8 +264,8 @@ class Blue(pygame.sprite.Sprite):
             self.turn = random.choice([2, 4])
             if self.rect.y <= 5:
                 self.rect.y = 6
-            elif self.rect.y >= 352:
-                self.rect.y = 351
+            elif self.rect.y >= 452:
+                self.rect.y = 451
         elif pygame.sprite.spritecollideany(self, vertical_borders):
             self.turn = random.choice([1, 3])
             if self.rect.x <= 5:
@@ -295,46 +298,62 @@ class Border(pygame.sprite.Sprite):
             self.image = pygame.Surface([x2 - x1, 1])
             self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
+        self.image.fill(pygame.Color('white'))
+
 
 class Dot(pygame.sprite.Sprite):
     image = load_image("dot.png")
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, *param):
+        super().__init__(param[:-2])
         self.image = Dot.image
         self.image.set_colorkey(pygame.Color('white'))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = 80
-        self.rect.y = 80
-        self.const = (0, '')
+        self.rect.x = param[-2]
+        self.rect.y = param[-1]
 
 
 class SmallDot(pygame.sprite.Sprite):
     image = pygame.transform.scale(load_image("dot.png"), (10, 10))
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, *param):
+        super().__init__(param[:-2])
         self.image = SmallDot.image
         self.image.set_colorkey(pygame.Color('white'))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = 100
-        self.rect.y = 100
-        self.const = (0, '')
+        self.rect.x = param[-2]
+        self.rect.y = param[-1]
+
+
+class Heart(pygame.sprite.Sprite):
+    image = load_image('heart.png')
+
+    def __init__(self, *param):
+        super().__init__(*param[:-2])
+        self.image = Heart.image
+        self.image.set_colorkey(pygame.Color('white'))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = param[-2]
+        self.rect.y = param[-1]
 
 
 def the_game():
     global SCORE
 
     pygame.display.flip()
-    dot = Dot()
-    small_dot = SmallDot()
-    red = Red()
-    yellow = Yellow()
-    green = Green()
-    blue = Blue()
-    player = Player()
+    dot = Dot(43, 58)
+    small_dot = SmallDot(228, 229)
+    red = Red(10, 10)
+    yellow = Yellow(50, 10)
+    green = Green(90, 10)
+    blue = Blue(130, 10)
+    player = Player(200, 300)
+    heart1 = Heart(10, 500)
+    heart2 = Heart(40, 500)
+    heart3 = Heart(70, 500)
 
     all_sprites.add(dot)
     all_sprites.add(small_dot)
@@ -343,11 +362,14 @@ def the_game():
     all_sprites.add(green)
     all_sprites.add(blue)
     all_sprites.add(player)
+    all_sprites.add(heart1)
+    all_sprites.add(heart2)
+    all_sprites.add(heart3)
 
-    Border(5, 5, width - 5, 5)
-    Border(5, 400 - 5, width - 5, 400 - 5)
-    Border(5, 5, 5, 400 - 5)
-    Border(width - 5, 5, width - 5, 400 - 5)
+    b1 = Border(5, 5, width - 5, 5)
+    b2 = Border(5, 500 - 5, width - 5, 500 - 5)
+    b3 = Border(5, 5, 5, 500 - 5)
+    b4 = Border(width - 5, 5, width - 5, 500 - 5)
 
     ticks = 0
 
@@ -423,24 +445,6 @@ def the_game():
         else:
             ticks -= 1
 
-        heart1_image = load_image('heart.png')
-        heart1_image.set_colorkey(pygame.Color('white'))
-        heart1_rect = heart1_image.get_rect()
-        heart1_rect.x = 10
-        heart1_rect.y = 500
-
-        heart2_image = load_image('heart.png')
-        heart2_image.set_colorkey(pygame.Color('white'))
-        heart2_rect = heart1_image.get_rect()
-        heart2_rect.x = 50
-        heart2_rect.y = 500
-
-        heart3_image = load_image('heart.png')
-        heart3_image.set_colorkey(pygame.Color('white'))
-        heart3_rect = heart1_image.get_rect()
-        heart3_rect.x = 90
-        heart3_rect.y = 500
-
         text = 'SCORE:' + str(SCORE)
         string_rendered = font.render(text, True, pygame.Color('white'))
         score_rect = string_rendered.get_rect()
@@ -450,9 +454,6 @@ def the_game():
         if flag:
             all_sprites.update()
         screen.fill(pygame.Color('black'))
-        screen.blit(heart1_image, heart1_rect)
-        screen.blit(heart2_image, heart2_rect)
-        screen.blit(heart3_image, heart3_rect)
         screen.blit(string_rendered, score_rect)
         all_sprites.draw(screen)
 
@@ -471,35 +472,35 @@ def database_query(query):
 
 def transition_screen(reverse=False):
     if reverse:
-        for i in range(26, 1, -1):
-            background = pygame.image.load(r"data/GUI/bck/" + str(i) + ".jpg")
-            screen.blit(background, (0, 0))
-            pygame.display.flip()
-            clock.tick(FPS)
+        rang = range(26, 1, -1)
     else:
-        for i in range(1, 26):
-            background = pygame.image.load(r"data/GUI/bck/" + str(i) + ".jpg")
-            screen.blit(background, (0, 0))
-            pygame.display.flip()
-            clock.tick(FPS)
+        rang = range(1, 26)
+
+    for i in rang:
+        background = pygame.image.load("data/GUI/bck/" + str(i) + ".jpg")
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 def records_screen():
-    records_screen_manager = pygame_gui.UIManager((600, 600), 'theme.json')
+    records_screen_manager = pygame_gui.UIManager((600, 600), 'data/theme.json')
+    scoring_list = sorted(database_query("SELECT * FROM Scoring"),
+                          key=lambda x: x[2], reverse=True)
+
     exit_button = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect((226, 545), (131, 42)),
         text='',
         manager=records_screen_manager,
-        object_id=ObjectID(class_id='@records_screen_buttons',
+        object_id=ObjectID(class_id='@records_screen',
                            object_id='#exit_button'))
-    scoring_list = sorted(database_query("SELECT * FROM Scoring"),
-                          key=lambda x: x[2], reverse=True)
-    if len(scoring_list) < 10:
-        count = len(scoring_list)
-    else:
-        count = 10
-    arcade_font = pygame.font.Font('data/fonts/arcade-n.ttf', 20)
-    if count != 0:
+
+    if scoring_list:
+
+        if len(scoring_list) < 10:
+            count = len(scoring_list)
+        else:
+            count = 10
         for i in range(count):
             text = arcade_font.render(
                 str(i + 1) + ' ' + scoring_list[i][1] + '  ' + str(
@@ -508,107 +509,123 @@ def records_screen():
     else:
         text = arcade_font.render('there is no any players!', 1, (180, 0, 0))
         screen.blit(text, (100, 250))
-    pygame.display.flip()
+
     running = True
     while running:
+
         events = pygame.event.get()
         for event in events:
+
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame_gui.UI_BUTTON_PRESSED:
+
                 if event.ui_element == exit_button:
                     transition_screen()
-                    transition_screen(True)
+                    transition_screen(reverse=True)
                     start_screen()
                     running = False
+
             records_screen_manager.process_events(event)
+
         records_screen_manager.update(FPS)
         records_screen_manager.draw_ui(screen)
 
         pygame.display.flip()
-        clock.tick(FPS)
 
 
 def init_screen():
-    running = True
-    init_screen_manager = pygame_gui.UIManager((600, 600), 'theme.json')
-    arcade_font = pygame.font.Font('data/fonts/arcade-n.ttf', 26)
+    init_screen_manager = pygame_gui.UIManager((600, 600), 'data/theme.json')
+
     text = arcade_font.render('Enter your name', 1, (180, 0, 0))
+
     text_input = pygame_gui.elements.UITextEntryLine(
         relative_rect=pygame.Rect((140, 240), (300, 60)),
         manager=init_screen_manager,
-        object_id=ObjectID(class_id='@init_screen_buttons',
+        object_id=ObjectID(class_id='@init_screen',
                            object_id='#text_box'))
+
     text_input.set_text_length_limit(10)
-    background = pygame.image.load(r"data/GUI/background.png")
-    screen.blit(background, (0, 0))
+
+    running = True
     while running:
         screen.blit(background, (0, 0))
+        screen.blit(text, (145, 150))
+
         events = pygame.event.get()
-        screen.blit(text, (110, 150))
         for event in events:
+
             if event.type == pygame.QUIT:
                 running = False
+
             if event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_RETURN and text_input.get_text() != '':
                     player_name = text_input.get_text().lower()
                     player_info = database_query("SELECT * FROM Scoring "
                                                  "WHERE PLAYER_NAME = "
                                                  f"'{player_name}'")
                     print(player_info)
-                    if player_info:
-                        the_game()
-                    else:
+
+                    if not player_info:
                         database_query("INSERT INTO Scoring(PLAYER_NAME) "
                                        f"VALUES('{player_name}')")
                     running = False
                     transition_screen()
                     the_game()
+
             init_screen_manager.process_events(event)
+
         init_screen_manager.update(FPS)
         init_screen_manager.draw_ui(screen)
         pygame.display.flip()
 
 
 def start_screen():
-    start_screen_manager = pygame_gui.UIManager((600, 600), 'theme.json')
+    start_screen_manager = pygame_gui.UIManager((600, 600), 'data/theme.json')
+
     start_button = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect((175, 120), (232, 76)),
         text='',
         manager=start_screen_manager,
-        object_id=ObjectID(class_id='@start_screen_buttons',
+        object_id=ObjectID(class_id='@start_screen',
                            object_id='#start_button'))
 
     records_button = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect((175, 220), (232, 76)),
         text='',
         manager=start_screen_manager,
-        object_id=ObjectID(class_id='@start_screen_buttons',
+        object_id=ObjectID(class_id='@start_screen',
                            object_id='#records_button'))
-    background = pygame.image.load(r"data/GUI/background.png")
+
     screen.blit(background, (0, 0))
+
     running = True
     while running:
+
         events = pygame.event.get()
         for event in events:
+
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame_gui.UI_BUTTON_PRESSED:
+
                 if event.ui_element == start_button:
-                    transition_screen()
                     running = False
+                    transition_screen()
+                    transition_screen(reverse=True)
                     init_screen()
                 elif event.ui_element == records_button:
-                    transition_screen()
-                    transition_screen(True)
                     running = False
+                    transition_screen()
+                    transition_screen(reverse=True)
                     records_screen()
+
             start_screen_manager.process_events(event)
+
         start_screen_manager.update(FPS)
         start_screen_manager.draw_ui(screen)
-
         pygame.display.flip()
-        clock.tick(FPS)
 
 
 start_screen()
